fefcc6c2f20c4a687ecd5210d191152ee34354b1
Fill84 <phil.pelzer@gmail.com>
2026-07-29 02:40:58 +0200

    test(core): scaffold Core and Tests projects with failing SizeParser tests

---
+++ tests/NoMercy.Plugin.TorrentDownloader.Core.Tests/Releases/SizeParserTests.cs
@@ -0,0 +1,36 @@
+using FluentAssertions;
+using NoMercy.Plugin.TorrentDownloader.Core.Releases;
+using Xunit;
+
+namespace NoMercy.Plugin.TorrentDownloader.Core.Tests.Releases;
+
+public class SizeParserTests
+{
+    [Theory]
+    [InlineData("1.4 GB", 1503238553L)]
+    [InlineData("700 MB", 734003200L)]
+    [InlineData("2.5GB", 2684354560L)]
+    [InlineData("1,234 MB", 1293942784L)]
+    [InlineData("4 TB", 4398046511104L)]
+    [InlineData("512 KB", 524288L)]
+    public void Parse_ReadsUnitAndValue(string text, long expected)
+    {
+        SizeParser.Parse(text).Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("N/A")]
+    [InlineData("unknown")]
+    public void Parse_ReturnsZeroWhenNothingParses(string? text)
+    {
+        SizeParser.Parse(text).Should().Be(0L);
+    }
+
+    [Fact]
+    public void Parse_PrefersLongestUnitSoGigabytesAreNotReadAsBytes()
+    {
+        SizeParser.Parse("1.4 GB").Should().BeGreaterThan(SizeParser.Parse("1.4 MB"));
+    }
+}
